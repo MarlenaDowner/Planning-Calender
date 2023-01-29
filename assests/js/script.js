9608dc4c708f3d580a648b82131c3d817fa78ccb
MarlenaDowner <119687938+MarlenaDowner@users.noreply.github.com>
2023-01-29 17:44:06 +0000

    adding Color-code each timeblock based on past, present, and future

--- assests/js/script.js
+++ assests/js/script.js
@@ -1,7 +1,7 @@
 //Link HTML elements to JS
 var currentDayE1 = $('#currentDay');
 
-// var timeDisplayEl = $('#time-display');
+//Need to make the boxes highlight depending on pass present or future
 
 //displaying the date at the top of the page 
 //NOTE: Decided to put the time and the date considering its a planner.
@@ -12,12 +12,4 @@ function lead() {
     currentDayE1.text(rightNow);
   }
   
-  setInterval(lead, 1000);
-
-//   // handle displaying the time
-//   //NOTE: In the UK we display the date as Monday 13th December (dddd Do MMMM),for this assessment I have kept to this format rather then the US format
-// function displayTime() {
-//     var rightNow = moment().format('DD MMM YYYY [at] hh:mm:ss a');
-//     timeDisplayEl.text(rightNow);
-//   }
-  
+  setInterval(lead, 1);
