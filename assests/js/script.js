69c131eaa19006dad0cbe16bab557ab625f815d2
MarlenaDowner <119687938+MarlenaDowner@users.noreply.github.com>
2023-01-30 10:07:54 +0000

    added id=save-btn to button class, so that I can try an create an event for Q4 to then save to local storage

--- assests/js/script.js
+++ assests/js/script.js
@@ -3,6 +3,8 @@ var currentDayE1 = $('#currentDay');
 
 //Need to make the boxes highlight depending on pass present or future
 
+let rows = document.getElementById("time");
+
 //displaying the date at the top of the page 
 //NOTE: Decided to put the time and the date considering its a planner.
 //NOTE: In the UK we display the date as Monday 13th December (dddd Do MMMM),for this assessment I have kept to this format rather then the US format
